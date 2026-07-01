20ac45ff535979702f10fc3ff3f9315371d37689
codingallmylife <216773305+codingallmylife@users.noreply.github.com>
2026-07-01 21:20:50 +0300

    Добавляет все 3 функции из домашнего задания

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+function checkStringLength(string, maxStringLength) {
+  return (string.length <= maxStringLength);
+}
+
+function isPalindrome(string) {
+  const normalizedString = (string.replaceAll(' ', '')).toLowerCase();
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return (reversedString === normalizedString);
+}
+
+function getNumberFromString(inputString) {
+  const string = inputString.toString();
+  let digits = '';
+  for (let i = 0; i < string.length; i++) {
+    const parsedChar = parseInt(string[i], 10);
+    if (!Number.isNaN(parsedChar)) {
+      digits += parsedChar;
+    }
+  }
+  return(parseInt(digits, 10));
+}
